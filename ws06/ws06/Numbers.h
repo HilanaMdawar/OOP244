a42940ea4291f4bb6a61c305169e25d0c229d0bf
Hilana Mdawar <nane90012@gmail.com>
2026-03-12 13:52:02 -0400

    done

--- ws06/ws06/Numbers.h
+++ ws06/ws06/Numbers.h
@@ -1,3 +1,19 @@
+/*************************************************************************************
+* OOP244 - 2261
+*
+* Student Name : Hilana Mdawar
+* Student Email : hmdawar@myseneca.ca
+* Workshop #  : 6
+* Course/Section: OOP244/NRA
+*
+* I declare that this assignment is my own work in accordance with the Seneca Academic
+* Policy. No part of this assignment has been copied manually or electronically from/to
+* any other source (including web sites) or distributed to other students.
+*
+* I have done all the coding by myself and only copied the code that my professor
+* provided to complete my workshops and assignments.
+*
+**************************************************************************************/
 #ifndef SENECA_NUMBERS_H
 #define SENECA_NUMBERS_H
 
